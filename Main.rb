9a2495e54e26cbf574ebbad596338645afca1a8c
Valeria Rodriguez <valery-rod@hotmail.com>
2018-10-15 17:20:54 -0500

    ShowMenu
    added 3 differents options in the class main

--- Main.rb
+++ Main.rb
@@ -1,14 +1,24 @@
-require './Simulation'
+require './Client'
 class Main
   def initialize
 
   end
   def showMenu
-
+  	puts '     Bienvenido al simulador de filas     \n1.Simulador de fila unitaria\n2.Simulador de multifilas\n3.Salir'
+  	option = gets.chomp.to_i
+  	runOption(option)
   end
   def runOption(option)
-
+  	case option
+  	when 1
+  		
+  	when 2
+  		
+  	when 3
+  		!exit
+  	end
   end
 end
 main = new Main
 main.showMenu
+
