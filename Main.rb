9a012a248c190ff7ed9fa8c8d410a53ca4abd8b9
Valeria Rodriguez <valery-rod@hotmail.com>
2018-10-15 19:45:44 -0500

    Input and menu
    added corrections in the menu and added an input

--- Main.rb
+++ Main.rb
@@ -4,21 +4,32 @@ class Main
 
   end
   def showMenu
-  	puts '     Bienvenido al simulador de filas     \n1.Simulador de fila unitaria\n2.Simulador de multifilas\n3.Salir'
-  	option = gets.chomp.to_i
-  	runOption(option)
+    puts '     Bienvenido al simulador de filas     '
+    puts '\n1.Simulador de fila unitaria\n2.Simulador de multifilas\n3.Salir\nDigite la opcion'
+    option = gets.chomp.to_i
+
+      runOption(option)
+    
   end
+  def input
+    puts 'Por favor digite la cantidad de cajas a usar'
+    boxes = gets.chomp.to_i
+    puts 'Por favor digite el delta de tiempo'
+    deltaTime = gets.chomp.to_i   
+  end
+
   def runOption(option)
-  	case option
-  	when 1
-  		
-  	when 2
-  		
-  	when 3
-  		!exit
-  	end
+    case option
+    when 1
+      input     
+    when 2
+      input   
+    when 3
+      !exit
+    else
+      showMenu
+    end
   end
 end
 main = new Main
 main.showMenu
-
