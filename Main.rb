630eb9b807658684685a7748f77172523b2f47d7
Valeria Rodriguez <valery-rod@hotmail.com>
2018-10-18 15:16:56 -0500

    Modified the input in class Main

--- Main.rb
+++ Main.rb
@@ -1,34 +1,40 @@
-require './Client'
+require './Simulation'
+
 class Main
   def initialize
-    @boxes = 0
-    @deltaTime = 0
+    @simulation = Simulation.new
   end
 
   def showMenu
     puts '     Bienvenido al simulador de filas     '
     puts "\n1.Simulador de fila unitaria\n2.Simulador de multifilas\n3.Salir\nDigite la opcion"
     option = gets.chomp.to_i
-
-      runOption(option)
-    
+    runOption(option)
   end
+
   def input
     puts 'Por favor digite la cantidad de cajas a usar'
-    @boxes = gets.chomp.to_i
-    puts 'Por favor digite el delta de tiempo'
-    @deltaTime = gets.chomp.to_i   
+    boxes = gets.chomp.to_i
+    #puts 'Por favor digite el delta de tiempo'
+    #deltaTime = gets.chomp.to_i
+    puts 'Por favor digite la duracion de la simulacion'
+    time = gets.chomp.to_i
+    @similation = Simulation.new(time)
+    @simulation.setCashRegisterNumber(boxes)
   end
 
   def runOption(option)
     case option
     when 1
-      input     
+      input
+      @simulation.runSimulationMultipleRows
     when 2
-      input   
+      input
+      @simulation.runSimulationSingleRow
     when 3
       !exit
     else
+      puts 'Opcion incorrecta'
       showMenu
     end
   end
