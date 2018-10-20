487e9f0a15eecab37a8c525e6f9c1396d0fef578
ValeriaRodriguez <valery-rod@hotmail.com>
2018-10-20 02:04:14 -0500

    added one  method for CashRegisters

--- SimulationSingleQueue.rb
+++ SimulationSingleQueue.rb
@@ -5,14 +5,17 @@ class SimulationSingleQueue
 
   def initialize(cashRegisters, totalTime, deltaTime)
     @cashRegisters=Array.new(cashRegisters)
-    for i in (0..(cashRegisters-1))
-      #temp = CashRegister.new
-      @cashRegisters[i] = CashRegister.new
-    end
+    initializeCashRegisters(cashRegisters)
     @totalTime=totalTime
     @deltaTime=deltaTime
     @currentTime = 0
     @clientsQueue=Array.new
+  end
+
+  def initializeCashRegisters(cashRegisters)
+    for i in (0..(cashRegisters-1))
+      @cashRegisters[i] = CashRegister.new
+    end
   end
 
   def clientsArrive
